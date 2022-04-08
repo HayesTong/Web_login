ea65b89c5a5a6c6e3f4f9ad14f7f7404d52b5689
HayesTong <102750516+HayesTong@users.noreply.github.com>
2022-04-08 21:57:01 +0800

    Add files via upload

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="FlipClock">
-	<h2>
-		{{state.name}}
-	</h2>
+    <h3>
+      <i class="el-icon-user"></i>
+      {{ show }}
+    </h3>
     <Flipper ref="flipperYear1" />
     <Flipper ref="flipperYear2" />
     <Flipper ref="flipperYear3" />
@@ -26,52 +27,52 @@
 </template>
 
 <script>
-import Flipper from './Flipper'
-import store from "./store.js"
+import Flipper from "./Flipper";
+// import store from "./store.js";
 export default {
-  name: 'FlipClock',
-  props:{
-    format:{
+  name: "FlipClock",
+  props: {
+    format: {
       type: String,
-      default: 'yyyymmddhhiiss'
-    }
+      default: "yyyymmddhhiiss",
+    },
   },
   data() {
     return {
       timer: null,
       flipObjs: [],
-	state:store.state
-    }
+      show: window.$cookies.get("username"),
+    };
   },
   components: {
-    Flipper
+    Flipper,
   },
   methods: {
     // 初始化数字
     init() {
-      let now = new Date()
-      let nowTimeStr = this.formatDate(new Date(now.getTime()), this.format)
-       for (let i = 0; i < this.flipObjs.length; i++) {
-         this.flipObjs[i].setFront(nowTimeStr[i])
-       }
+      let now = new Date();
+      let nowTimeStr = this.formatDate(new Date(now.getTime()), this.format);
+      for (let i = 0; i < this.flipObjs.length; i++) {
+        this.flipObjs[i].setFront(nowTimeStr[i]);
+      }
     },
     // 开始计时
     run() {
       this.timer = setInterval(() => {
         // 获取当前时间
-        let now = new Date()
-        let nowTimeStr = this.formatDate(new Date(now.getTime() - 1000), this.format)
-        let nextTimeStr = this.formatDate(now, this.format)
+        let now = new Date();
+        let nowTimeStr = this.formatDate(
+          new Date(now.getTime() - 1000),
+          this.format
+        );
+        let nextTimeStr = this.formatDate(now, this.format);
         for (let i = 0; i < this.flipObjs.length; i++) {
           if (nowTimeStr[i] === nextTimeStr[i]) {
-            continue
+            continue;
           }
-          this.flipObjs[i].flipDown(
-            nowTimeStr[i],
-            nextTimeStr[i]
-          )
+          this.flipObjs[i].flipDown(nowTimeStr[i], nextTimeStr[i]);
         }
-      }, 1000)
+      }, 1000);
     },
     // 正则格式化日期
     formatDate(date, dateFormat) {
@@ -83,21 +84,21 @@ export default {
       if (/(y+)/.test(dateFormat)) {
         dateFormat = dateFormat.replace(
           RegExp.$1,
-          (date.getFullYear() + '').substr(4 - RegExp.$1.length)
-        )
+          (date.getFullYear() + "").substr(4 - RegExp.$1.length)
+        );
       }
       // 格式化月、日、时、分、秒
       let o = {
-        'm+': date.getMonth() + 1,
-        'd+': date.getDate(),
-        'h+': date.getHours(),
-        'i+': date.getMinutes(),
-        's+': date.getSeconds()
-      }
+        "m+": date.getMonth() + 1,
+        "d+": date.getDate(),
+        "h+": date.getHours(),
+        "i+": date.getMinutes(),
+        "s+": date.getSeconds(),
+      };
       for (let k in o) {
         if (new RegExp(`(${k})`).test(dateFormat)) {
           // 取出对应的值
-          let str = o[k] + ''
+          let str = o[k] + "";
           /* 根据设置的格式，输出对应的字符
            * 例如: 早上8时，hh => 08，h => 8
            * 但是，当数字>=10时，无论格式为一位还是多位，不做截取，这是与年份格式化不一致的地方
@@ -106,15 +107,15 @@ export default {
           dateFormat = dateFormat.replace(
             RegExp.$1,
             RegExp.$1.length === 1 ? str : this.padLeftZero(str)
-          )
+          );
         }
       }
-      return dateFormat
+      return dateFormat;
     },
     // 日期时间补零
     padLeftZero(str) {
-      return ('00' + str).substr(str.length)
-    }
+      return ("00" + str).substr(str.length);
+    },
   },
   mounted() {
     this.flipObjs = [
@@ -131,27 +132,27 @@ export default {
       this.$refs.flipperMinute1,
       this.$refs.flipperMinute2,
       this.$refs.flipperSecond1,
-      this.$refs.flipperSecond2
-    ]
-    this.init()
-    this.run()
-  }
-}
+      this.$refs.flipperSecond2,
+    ];
+    this.init();
+    this.run();
+  },
+};
 </script>
 
 <style>
 .FlipClock {
-    text-align: center;
-    line-height: 200px;
+  text-align: center;
+  line-height: 200px;
 }
 .FlipClock .M-Flipper {
-    margin: 0 0px;
+  margin: 0 0px;
 }
 .FlipClock em {
-    display: inline-block;
-    line-height: 102px;
-    font-size: 66px;
-    font-style: normal;
-    vertical-align: top;
+  display: inline-block;
+  line-height: 102px;
+  font-size: 66px;
+  font-style: normal;
+  vertical-align: top;
 }
 </style>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -46,11 +46,11 @@
 
 <script>
 import axios from "axios";
-import store from "./store.js"
+// import store from "./store.js";
 export default {
   data() {
     return {
-		name:"",
+      name: "",
       loginform: {
         username: "666",
         password: "",
@@ -90,9 +90,11 @@ export default {
           });
         } else {
           if (valid) {
-            axios.post("http://127.0.0.1:3000/dataPost", this.loginform, {
-			'Content-Type':'application/x-www-form-urlencoded','Access-Control-Allow-Origin':'*',
-			})
+            axios
+              .post("http://127.0.0.1:3000/dataPost", this.loginform, {
+                "Content-Type": "application/x-www-form-urlencoded",
+                "Access-Control-Allow-Origin": "*",
+              })
               .then((res) => {
                 // console.log(res)
                 // console.log(res.data.code)
@@ -106,11 +108,11 @@ export default {
 
                     // this.$cookieStore.setCookie( 'username' ,this.username,20);
                     // console.log(this.$cookieStore.setCookie)
-                    this.$cookies.set('username' ,res.data.code,20);
-					this.$router.push("/");
-                    console.log(this.$cookies.get("username"))
-					store.setStateMessage(res.data.username);
-					console.log(store.state.name)
+                    this.$cookies.set("username", res.data.username, 20);
+                    this.$router.push("/");
+                    console.log(this.$cookies.get("username"));
+                    // store.setStateMessage(res.data.username);
+                    // console.log(store.state.name);
                     break;
 
                   case 555:
